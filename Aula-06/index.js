3e1bbdcf076935870ed2cfcab988071145d1e275
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-03-14 20:53:41 -0300

    aula6 estrutura padrão

---
+++ Aula-06/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* app.get('/', function (req, res) {
+	res.send('Hello World');
+}); */
+
+app.get('/', function (req, res) {
+	res.render('index');
+});
+app.post('/', function (req, res) {
+	res.send('Post');
+});
+
+app.get('/pagina', function (req, res) {
+	res.send('Pagina2');
+});
+
+// Adicionando a const port e uma arow function de callback para mostrar no console que o servidor está rodando.
+app.listen(port, () =>
+	console.log(`Servidor rodando em http://localhost:${port}`)
+);
