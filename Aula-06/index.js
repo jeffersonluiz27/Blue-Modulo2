3e441e7fb1f4f8f11d0cdce41f328df6960333f0
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-03-14 21:04:00 -0300

    template blue

--- Aula-06/index.js
+++ Aula-06/index.js
@@ -5,11 +5,11 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* app.get('/', function (req, res) {
-	res.send('Hello World');
-}); */
-
 app.get('/', function (req, res) {
+	res.send('Hello World');
+});
+
+app.get('/index', function (req, res) {
 	res.render('index');
 });
 app.post('/', function (req, res) {
