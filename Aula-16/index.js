a6539f6ee4b6041e62a77013efd4241956930280
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-03-28 19:52:08 -0300

    repositorio inicial

---
+++ Aula-16/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config;
+
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+
+const filmes = [
+	{
+		id: 1,
+		nome: 'Harry Potter e a Pedra Filosofal',
+		descricao: 'O filme do Harry Potter',
+		imagem: 'https://ingresso-a.akamaihd.net/img/cinema/cartaz/7766-cartaz.jpg',
+	},
+	{
+		id: 2,
+		nome: 'Senhor dos Anéis: As Duas Torres',
+		descricao: 'O filme do Sr. Dos Anéis',
+		imagem:
+			'https://i.pinimg.com/originals/e5/e8/cf/e5e8cfc267a11c8ae6ba728b4537543f.jpg',
+	},
+];
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		filmes,
+	});
+});
+
+app.listen(port, () =>
+	console.log(`Servidor rodando em http://localhost:${port}`)
+);
