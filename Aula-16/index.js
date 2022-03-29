c6cc6a2af0ece1f2995d386cb45735d03238a320
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-03-28 23:00:08 -0300

    select css detalhes

--- Aula-16/index.js
+++ Aula-16/index.js
@@ -1,36 +1,28 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-require('dotenv').config;
+require('dotenv').config();
 const Filme = require('./model/filme');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded());
-
-/* const filmes = [
-	{
-		id: 1,
-		nome: 'Harry Potter e a Pedra Filosofal',
-		descricao: 'O filme do Harry Potter',
-		imagem: 'https://ingresso-a.akamaihd.net/img/cinema/cartaz/7766-cartaz.jpg',
-	},
-	{
-		id: 2,
-		nome: 'Senhor dos Anéis: As Duas Torres',
-		descricao: 'O filme do Sr. Dos Anéis',
-		imagem:
-			'https://i.pinimg.com/originals/e5/e8/cf/e5e8cfc267a11c8ae6ba728b4537543f.jpg',
-	},
-]; */
 
 app.get('/', async (req, res) => {
 	const filmes = await Filme.findAll();
 
 	res.render('index', {
 		filmes,
+	});
+});
+
+app.get('/filmes/:id', async (req, res) => {
+	const filme = await Filme.findByPk(req.params.id);
+
+	res.render('detalhes', {
+		filme,
 	});
 });
 
